90c5ec75231d277d36a605cd8981881292468e28
Fanny Jestin <f.jestin@tbs-education.org>
2019-10-28 12:06:39 +0100

    first version

---
+++ test/models/user_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  belongs_to :city
+  has_many :gossips
+  has_many :comments
+  has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
+  has_many :received_messages, foreign_key: 'recipient_id', class_name: "RecipientList"
+end
